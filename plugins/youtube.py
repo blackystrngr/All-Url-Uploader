2b98c7380d429b1cf059c48bb847da1e4eea7847
blackystrngr <kisalk2007@outlook.com>
2026-01-18 13:33:48 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,7 +1,7 @@
 import os
 import asyncio
 
-from yt_dlp import YoutubeDL  # Switched to yt-dlp for active maintenance and better compatibility
+from yt_dlp import YoutubeDL  # Use yt-dlp for better support and maintenance
 from pyrogram import enums
 from pyrogram.types import Message
 from pyrogram import Client, filters
@@ -18,16 +18,14 @@ async def callback_query_ytdl_audio(_, callback_query):
             "format": "bestaudio",
             "outtmpl": "%(title)s - %(extractor)s-%(id)s.%(ext)s",
             "writethumbnail": True,
-            'cookiefile': 'cookies.txt',  # Added to bypass YouTube restrictions
+            'cookiefile': 'cookies.txt',  # For bypassing YouTube restrictions
         }
         with YoutubeDL(ydl_opts) as ydl:
             message = callback_query.message
             await message.reply_chat_action(enums.ChatAction.TYPING)
             info_dict = ydl.extract_info(url, download=False)
-            # download
             await callback_query.edit_message_text("**Downloading audio...**")
             ydl.process_info(info_dict)
-            # upload
             audio_file = ydl.prepare_filename(info_dict)
             task = asyncio.create_task(send_audio(message, info_dict, audio_file))
             while not task.done():
@@ -42,15 +40,18 @@ async def callback_query_ytdl_audio(_, callback_query):
 
 async def send_audio(message: Message, info_dict, audio_file):
     basename = audio_file.rsplit(".", 1)[-2]
-    if info_dict["ext"] == "webm":
+    if info_dict.get("ext") == "webm":
         audio_file_weba = f"{basename}.weba"
         os.rename(audio_file, audio_file_weba)
         audio_file = audio_file_weba
-    thumbnail_url = info_dict["thumbnail"]
-    thumbnail_file = f"{basename}.{get_file_extension_from_url(thumbnail_url)}"
+    thumbnail_url = info_dict.get("thumbnail")
+    if thumbnail_url:
+        thumbnail_file = f"{basename}.{get_file_extension_from_url(thumbnail_url)}"
+    else:
+        thumbnail_file = None
     download_location = f"{Config.DOWNLOAD_LOCATION}/{message.from_user.id}.jpg"
     thumb = download_location if os.path.isfile(download_location) else None
-    webpage_url = info_dict["webpage_url"]
+    webpage_url = info_dict.get("webpage_url", "")
     title = info_dict.get("title", "")
     caption = f'<b><a href="{webpage_url}">{title}</a></b>'
     duration = int(float(info_dict.get("duration", 0)))
@@ -64,16 +65,23 @@ async def send_audio(message: Message, info_dict, audio_file):
         parse_mode=enums.ParseMode.HTML,
         thumb=thumb,
     )
-    os.remove(audio_file)
-    os.remove(thumbnail_file)
+    try:
+        os.remove(audio_file)
+        if thumbnail_file:
+            os.remove(thumbnail_file)
+    except FileNotFoundError:
+        pass  # Ignore if file not found
 
 async def send_video(message: Message, info_dict, video_file):
     basename = video_file.rsplit(".", 1)[-2]
-    thumbnail_url = info_dict["thumbnail"]
-    thumbnail_file = f"{basename}.{get_file_extension_from_url(thumbnail_url)}"
+    thumbnail_url = info_dict.get("thumbnail")
+    if thumbnail_url:
+        thumbnail_file = f"{basename}.{get_file_extension_from_url(thumbnail_url)}"
+    else:
+        thumbnail_file = None
     download_location = f"{Config.DOWNLOAD_LOCATION}/{message.from_user.id}.jpg"
     thumb = download_location if os.path.isfile(download_location) else None
-    webpage_url = info_dict["webpage_url"]
+    webpage_url = info_dict.get("webpage_url", "")
     title = info_dict.get("title", "")
     caption = f'<b><a href="{webpage_url}">{title}</a></b>'
     duration = int(float(info_dict.get("duration", 0)))
@@ -87,8 +95,12 @@ async def send_video(message: Message, info_dict, video_file):
         parse_mode=enums.ParseMode.HTML,
         thumb=thumb,
     )
-    os.remove(video_file)
-    os.remove(thumbnail_file)
+    try:
+        os.remove(video_file)
+        if thumbnail_file:
+            os.remove(thumbnail_file)
+    except FileNotFoundError:
+        pass  # Ignore if file not found
 
 @Client.on_callback_query(filters.regex("^ytdl_video$"))
 async def callback_query_ytdl_video(_, callback_query):
@@ -98,16 +110,14 @@ async def callback_query_ytdl_video(_, callback_query):
             "format": "best[ext=mp4]",
             "outtmpl": "%(title)s - %(extractor)s-%(id)s.%(ext)s",
             "writethumbnail": True,
-            'cookiefile': 'cookies.txt',  # Added to bypass YouTube restrictions
+            'cookiefile': 'cookies.txt',  # For bypassing YouTube restrictions
         }
         with YoutubeDL(ydl_opts) as ydl:
             message = callback_query.message
             await message.reply_chat_action(enums.ChatAction.TYPING)
             info_dict = ydl.extract_info(url, download=False)
-            # download
             await callback_query.edit_message_text("**Downloading video...**")
             ydl.process_info(info_dict)
-            # upload
             video_file = ydl.prepare_filename(info_dict)
             task = asyncio.create_task(send_video(message, info_dict, video_file))
             while not task.done():
